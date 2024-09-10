323cc78c98ed90550770083162268c63878babb7
Raphael Hagen <norlandrhagen@gmail.com>
2024-09-10 13:13:13 -0600

    cleanup

--- feedstock/godas.py
+++ feedstock/godas.py
@@ -37,15 +37,8 @@ multi_level_vars = [
     "vcur",  # multi
 ]
 
-# does full surface work?
-# no, fails with: ValueError: Region (slßice(324, 340, None), slice(0, 209, None), slice(180, 360, None), slice(None, None, None)) does not align with Zarr chunks (20, 209, 180, 1). [while running 'Create|OpenURLWithFSSpec|OpenWithXarray|Preprocess|StoreToZarr|ConsolidateDimensionCoordinates|ConsolidateMetadata|Copy/StoreToZarr/StoreDatasetFragments/Map(store_dataset_fragment)']
-# does full multi work?
-# Data output stream buffer size 580033055 exceeds 536870912 bytes. This is likely due to a large element in a PCollection. Large elements increase pipeline RAM requirements and can cause runtime errors. Prefer multiple small elements over single large elements in PCollections. If needed, store large blobs in external storage systems, and use PCollections to pass their metadata, or use a custom coder that reduces the element's size.
 
-# test 2 years
-# test 2 vars all years
-
-years = range(1980, 2023)
+years = range(1980, 2024)
 
 
 def make_full_path(variable, time):
@@ -59,26 +52,16 @@ time_concat_dim = ConcatDim("time", years)
 
 
 class Preprocess(beam.PTransform):
-    """Custom transform to promote vars as coords"""
+    """Custom transform to remove date&timePlot"""
 
     @staticmethod
     def _set_bnds_as_coords(ds: xr.Dataset) -> xr.Dataset:
-        # if "level" not in ds.dims:  # in ds?
-        #     ds = ds.transpose("time", "lat", "lon")
-        # else:
-        #     ds = ds.transpose("time", "lat", "lon", "level")
-
-        # # if levels not present, assign surface level
-        # ds = ds.expand_dims("level").assign_coords(level=("level", [0.0]))
         ds = ds.drop_vars(["date", "timePlot"])
-        # new_coords_vars = ["date", "timePlot"]
-        # ds = ds.set_coords(new_coords_vars)
-        # import pdb; pdb.set_trace()
 
         return ds
 
     def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
-        return pcoll | "Sets bands as coords" >> beam.MapTuple(
+        return pcoll | "Drops vars" >> beam.MapTuple(
             lambda k, v: (k, self._set_bnds_as_coords(v))
         )
 
@@ -97,13 +80,13 @@ GODASsurface = (
     | OpenWithXarray(file_type=pattern_surface.file_type)
     | Preprocess()
     | StoreToZarr(
-        target_chunks={"time": 20, "lat": 209, "lon": 180},
         store_name="GODAS_surface_level.zarr",
+        target_chunks={"time": 720, "lat": 209, "lon": 180},
         combine_dims=pattern_surface.combine_dim_keys,
     )
     | ConsolidateDimensionCoordinates()
     | ConsolidateMetadata()
-    # | Copy(target=catalog_store_urls["surface"])
+    | Copy(target=catalog_store_urls["surface"])
 )
 
 GODASmulti = (
@@ -112,7 +95,7 @@ GODASmulti = (
     | OpenWithXarray(file_type=pattern_multi_lvl.file_type)
     | Preprocess()
     | StoreToZarr(
-        target_chunks={"time": 20, "lat": 209, "lon": 180, "level": 20},
+        target_chunks={"time": 36, "lat": 209, "lon": 180, "level": 20},
         store_name="GODAS_multiple_levels.zarr",
         combine_dims=pattern_multi_lvl.combine_dim_keys,
     )
