9c1aa9fa0a7ebfd928ad110388bab3924a838e7b
Raphael Hagen <norlandrhagen@gmail.com>
2024-09-10 13:30:50 -0600

    typo

--- feedstock/godas.py
+++ feedstock/godas.py
@@ -1,5 +1,4 @@
 # pangeo-forge-runner bake --config configs/config_local_hub.py  --repo='.' --Bake.job_name=test --Bake.recipe_id 'GODAS_multi'
-import xarray as xr
 import apache_beam as beam
 from leap_data_management_utils.data_management_transforms import (
     get_catalog_store_urls,
@@ -51,7 +50,6 @@ variable_merge_dim_multi = MergeDim("variable", multi_level_vars)
 time_concat_dim = ConcatDim("time", years)
 
 
-
 pattern_surface = FilePattern(
     make_full_path, variable_merge_dim_surface, time_concat_dim, file_type="netcdf4"
 )
@@ -63,7 +61,10 @@ pattern_multi_lvl = FilePattern(
 GODASsurface = (
     beam.Create(pattern_surface.items())
     | OpenURLWithFSSpec()
-    | OpenWithXarray(file_type=pattern_surface.file_type, xarray_open_kwargs={'drop_variables':["date","timePlot"]})
+    | OpenWithXarray(
+        file_type=pattern_surface.file_type,
+        xarray_open_kwargs={"drop_variables": ["date", "timePlot"]},
+    )
     | StoreToZarr(
         store_name="GODAS_surface_level.zarr",
         target_chunks={"time": 720, "lat": 209, "lon": 180},
@@ -77,8 +78,10 @@ GODASsurface = (
 GODASmulti = (
     beam.Create(pattern_multi_lvl.items())
     | OpenURLWithFSSpec()
-    | OpenWithXarray(file_type=pattern_multi_lvl.file_type)
-    | Preprocess()
+    | OpenWithXarray(
+        file_type=pattern_multi_lvl.file_type,
+        xarray_open_kwargs={"drop_variables": ["date", "timePlot"]},
+    )
     | StoreToZarr(
         target_chunks={"time": 36, "lat": 209, "lon": 180, "level": 20},
         store_name="GODAS_multiple_levels.zarr",
